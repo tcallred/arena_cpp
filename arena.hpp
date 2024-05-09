8548b77dc43ab6cfd8e7684f79371238c4142c62
Taylor Allred <taylor.allred21@gmail.com>
2024-05-08 21:48:10 -0600

    webkit format; example passing arena as param

--- arena.hpp
+++ arena.hpp
@@ -1,30 +1,29 @@
 #pragma once
 #include <memory_resource>
-#include <vector>
 #include <string>
+#include <vector>
 
-class Arena
-{
+class Arena {
 public:
     template <typename T, typename... A>
-    T *make(A &&...args)
+    T* make(A&&... args)
     {
-        void *mem = m_store.allocate(sizeof(T), alignof(T));
-        return new (mem) T{(A &&)args...};
+        void* mem = m_store.allocate(sizeof(T), alignof(T));
+        return new (mem) T { (A&&)args... };
     };
 
     template <typename T, typename... A>
-    std::pmr::vector<T> make_vector(A &&...args)
+    std::pmr::vector<T> make_vector(A&&... args)
     {
-        std::pmr::polymorphic_allocator<int> pa{&m_store};
-        auto v = std::pmr::vector<T>{pa};
-        v = {(A &&)args...};
+        std::pmr::polymorphic_allocator<int> pa { &m_store };
+        auto v = std::pmr::vector<T> { pa };
+        v = { (A&&)args... };
         return v;
     }
 
-    std::pmr::string make_string(std::string &&s)
+    std::pmr::string make_string(std::string&& s)
     {
-        std::pmr::polymorphic_allocator<char> pa{&m_store};
+        std::pmr::polymorphic_allocator<char> pa { &m_store };
         auto strspace = std::pmr::string(pa);
         strspace = s;
         return strspace;
--- example.cpp
+++ example.cpp
@@ -2,46 +2,58 @@
 #include <iostream>
 #include <memory>
 
-struct Foo
-{
+struct Foo {
     int xx = 32;
     int yy = 5;
 };
 
 void makeTest()
 {
-    Arena scratch{};
-    int *x = scratch.make<int>();
+    Arena scratch {};
+    int* x = scratch.make<int>();
     *x = 42;
     std::cout << *x << '\n';
-    Foo *foo = scratch.make<Foo>();
+    Foo* foo = scratch.make<Foo>();
     std::cout << foo->xx << '\n';
-    Foo *bar = scratch.make<Foo>(21, 69);
+    Foo* bar = scratch.make<Foo>(21, 69);
     std::cout << bar->xx << '\n';
-    auto baz = scratch.make<Foo>(Foo{
+    auto baz = scratch.make<Foo>(Foo {
         .xx = 4,
-        .yy = 5});
+        .yy = 5 });
     std::cout << baz->xx << '\n';
 }
 
 void vectorTest()
 {
-    Arena scratch{};
+    Arena scratch {};
     auto myvec = scratch.make_vector<int>(42, 69);
     myvec.push_back(1);
     myvec.push_back(2);
     myvec.push_back(3);
-    for (auto i : myvec)
-    {
+    for (auto i : myvec) {
         std::cout << i << '\n';
     }
 }
 
 void stringTest()
 {
-    Arena scratch{};
+    Arena scratch {};
     auto mystr = scratch.make_string("hello");
     std::cout << mystr << '\n';
+}
+
+Foo* allocAFoo(Arena& arena)
+{
+    return arena.make<Foo>(55, 66);
+}
+
+void callAnAllocatingFunc()
+{
+    Arena scratch {};
+    auto foo1 = scratch.make<Foo>(11, 22);
+    auto foo2 = allocAFoo(scratch);
+    std::cout << foo1->xx << '\n';
+    std::cout << foo2->xx << '\n';
 }
 
 int main(void)
@@ -49,5 +61,6 @@ int main(void)
     makeTest();
     vectorTest();
     stringTest();
+    callAnAllocatingFunc();
     return 0;
 }
